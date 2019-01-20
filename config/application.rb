b0737d3ffba8cf7b69818cd0c0e5b7b0d22d2b09
song <songsk22366@naver.com>
2019-01-20 18:56:59 +0000

    heroku test2

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Propertys
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    #config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,9 +13,8 @@ Rails.application.configure do
   
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
-    config.action_controller.perform_caching = false
     #Rails.cache.clear
-    #config.action_controller.perform_caching = true
+    config.action_controller.perform_caching = true
     config.cache_store = :memory_store
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
