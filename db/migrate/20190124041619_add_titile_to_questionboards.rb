7abeb0a35b6bd06fd169c3815c354d95192b4e46
song <songsk22366@naver.com>
2019-01-24 05:14:52 +0000

    asd

--- db/migrate/20190124041619_add_titile_to_questionboards.rb
+++
@@ -1,7 +0,0 @@
-class AddTitileToQuestionboards < ActiveRecord::Migration[5.1]
-  def change
-    
-    add_column :questionboards, :title, :string
-
-  end
-end
--- db/migrate/20190124050920_remove_access_level_from_properties.rb
+++
@@ -1,8 +0,0 @@
-class RemoveAccessLevelFromProperties < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :properties, :count, :integer
-    remove_column :properties, :impressions_count, :integer
-    remove_column :properties, :property_image, :text
-    remove_column :properties, :video, :text
-  end
-end
