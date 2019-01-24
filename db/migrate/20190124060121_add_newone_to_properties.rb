4512d5fe2fcd329d38a14edd6a2002566ec94446
song <songsk22366@naver.com>
2019-01-24 06:10:16 +0000

    pm 15:10 error allclear

--- db/migrate/20190124060121_add_newone_to_properties.rb
+++
@@ -1,8 +0,0 @@
-class AddNewoneToProperties < ActiveRecord::Migration[5.1]
-  def change
-    add_column :properties, :subsidy, :string
-    add_column :properties, :reward, :string
-    add_column :properties, :region, :string
-    add_column :properties, :extent, :string
-  end
-end
